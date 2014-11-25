0a75c4d6769a2eea92c5099c019bec768592abb9
jfmdev <jfmdev@gmail.com>
2014-11-25 09:10:02 +0000

    First draft of DAL layer Time: 2h

--- code/js/addressbook.dal.js
+++ code/js/addressbook.dal.js
@@ -15,3 +15,62 @@
  *  You should have received a copy of the GNU General Public License
  *  along with ngAddressBook. If not, see <http://www.gnu.org/licenses/>.
  */
+
+// Create database.
+var myDb = new PouchDB('AddressBook');
+
+// Define namespace in which to include all database-related methods.
+var DAL = DAL || {};
+
+/**
+ * Fun function for retrieve all entries in the database.
+ */
+DAL.allFun = function (doc) {
+    emit(doc._id, doc);
+};
+
+/**
+ * Get the lists of contacts.
+ * 
+ * @param {function} callback The function that is called when the operation has been done.
+ */
+DAL.list = function (callback) {
+    myDb.query(DAL.allFun, callback);
+};
+
+/**
+ * Get a contact.
+ * 
+ * @param {string} id The contact's id.
+ * @param {function} callback The function that is called when the operation has been done.
+ * @returns {object} A contact
+ */
+DAL.get = function (id, callback) {
+    myDb.get(id, callback);
+};
+
+/**
+ * Deletes a contact.
+ * 
+ * @param {string} doc The contact to delete.
+ * @param {function} callback The function that is called when the operation has been done.
+ */
+DAL.delete = function (doc, callback) {
+    myDb.remove(doc._id, doc._rev, callback); 
+};
+
+/**
+ * Saves a contact.
+ * 
+ * @param {object} doc The contact's details.
+ * @param {function} callback The function that is called when the operation has been done.
+ */
+DAL.save = function (doc, callback) {
+    // Verify if the contact is new or is an update.
+    if(doc._id === null) {
+        doc._id = 'id.' + new Date().getTime();
+    }
+    
+    // Save contact.
+    myDb.put(doc, doc.id, callback);
+};
--- code/js/addressbook.main.js
+++ code/js/addressbook.main.js
@@ -25,53 +25,94 @@ addressBookApp.controller('BookCtrl', function ($scope) {
   
     // Get list of friends.
     $scope.friends = [
-        { id : 1,
+        { _id : 1,
         name : "John",
         email : "john@something.com",
         phone : "12 34 56 78",
         address : "Somewhere",
         relative : true },
-        { id : 2,
+        { _id : 2,
         name : "Jane",
         email : "jane@something.com",
         phone : "01 23 45 67",
         address : "Nowhere",
         relative : false }
     ];
+    $scope.friends = [];
   
     // Define object used for the edition form.
-    $scope.activeEntry = {
-        id : null,
-        name : "asdf",
-        email : "asdf",
-        phone : "asdf",
-        address : "adsf",
-        relative : false
-    };
+    $scope.activeEntry = {};
 
-    $scope.showList = function() {
-        $scope.appState = "list";
+    $scope.resetActiveEntry = function() {
+        $scope.activeEntry = {
+            _id : null,
+            name : "",
+            email : "",
+            phone : "",
+            address : "",
+            relative : false
+        };
+    };
+    $scope.resetActiveEntry();
+    
+    // TENGO QUE MOSTRAR UN MENU DE ESPERA CADA VEZ QUE SE ESPERE A UN CALLBACK
+    // TAL VEZ CON UN appState = 'loading'.
+    
+    $scope.showList = function(reload) {
+        if(reload === true) {
+            $scope.appState = "loading";
+            DAL.list(function(err, response) {
+                console.log("showList - " + err + " - " + response);
+                $scope.friends.length = 0;
+                for(var i=0; i<response.rows.length; i++) {
+                    $scope.friends.push(response.rows[i].value);
+                }
+                $scope.appState = "list";
+                $scope.$apply();
+            });
+        } else {
+            $scope.appState = "list";
+        }
         return false;
     };
-
+    $scope.showList(true);
+    
     $scope.deleteEntry = function() {
+        $scope.appState = "loading";
         var del = confirm("Are you sure that you want to delete this contact?")
         if(del) {
-            $scope.appState = "list";
+            DAL.delete($scope.activeEntry, function(err, response) {
+                $scope.showList(true);
+            });
         }
         return false;
     };
 
     $scope.saveEntry = function() {
-        $scope.appState = "list";
+        $scope.appState = "loading";
+        DAL.save($scope.activeEntry, function(res, doc) {
+            console.log("saveEntry " + res +" - " + doc + " -- " + Object.keys(doc) + " -- " + Object.keys($scope.activeEntry));
+            $scope.showList(true);
+        });
         return false;
     };
     
 
     $scope.editEntry = function(id) {
-        //alert(id);
-        $scope.activeEntry.id = id;
-        $scope.appState = "edit";
+        $scope.appState = "loading";
+        if(id !== null && id !== undefined) {
+            console.log("editEntry - update " + id);
+            DAL.get(id, function(err, doc) {
+               $scope.activeEntry = doc;
+               console.log("editEntry - update = " + doc);
+               $scope.appState = "edit";
+               $scope.$apply();
+            });            
+        } else {
+            console.log("editEntry - new");
+            $scope.resetActiveEntry();
+            $scope.appState = "edit";
+        }
         return false;
     };
 });
