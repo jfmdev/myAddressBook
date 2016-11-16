80ddb8737ff5d3a812df91b8bf11ed21337fbd2d
jfmdev <jfmdev@gmail.com>
2016-11-16 11:03:17 -0300

    Update project name

--- js/dal.js
+++ js/dal.js
@@ -1,19 +1,19 @@
 /*
  *  Copyright (C) 2014 Jose F. Maldonado
- *  This file is part of ngAddressBook.
+ *  This file is part of myAddressBook.
  *  
- *  ngAddressBook is free software: you can redistribute it and/or modify
+ *  myAddressBook is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  ngAddressBook is distributed in the hope that it will be useful,
+ *  myAddressBook is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with ngAddressBook. If not, see <http://www.gnu.org/licenses/>.
+ *  along with myAddressBook. If not, see <http://www.gnu.org/licenses/>.
  */
 // Declare module.
 var moduleSvc = angular.module('module.Dal', []);
